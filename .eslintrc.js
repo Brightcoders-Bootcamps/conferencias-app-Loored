2fc28a3e92215e9b9ad263f96716b9b2258d049e
Loored <loredhearsindie@gmail.com>
2020-07-09 22:24:10 -0500

    📝 Update eslint configuration and upgrade project structure
    🦾 implemented rules to the project and upgraded the project structure so it is better organized
    ❌ removed some components
    ⬆️ upgraded and added dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+  },
+  plugins: ['react', 'react-native'],
+  ecmaFeatures: {
+    jsx: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  rules: {
+    'react/no-did-mount-set-state': 2,
+    'react/no-direct-mutation-state': 2,
+    'react/jsx-uses-vars': 2,
+    'no-undef': 2,
+    semi: 2,
+    'react/prop-types': 2,
+    'react/jsx-no-bind': 2,
+    'react/jsx-no-duplicate-props': 2,
+    // we only want single quotes
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error'],
+  },
+  globals: {
+    GLOBAL: false,
+    it: false,
+    expect: false,
+    describe: false,
+  },
 };
--- App.js
+++ App.js
@@ -1,114 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+function HomeScreen ({navigation}) {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>HomeScreen</Text>
+      <Button 
+        title="Go to Conferences"
+        onPress={() => navigation.navigate('Conferences')}
+      />
+      <Button 
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile', {name: 'Profile'})}
+      />
+    </View>
+  );
+}
+
+const Conferences = ( {navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Conferences</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('HomeScreen')}
+      />
+      <Button title="Go back first screen stack" onPress={() => navigation.popToTop()}
+      />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+function Profile() {
+  return (
+    <View>
+      <Button title="Clc"
+        onPress={() => alert('hey there')}
+
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        headerMode="none"
+        screenOptions= {{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTitleAlign: 'center',
+          headerTintColor: '#fff',
+        }}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} 
+          options={{
+            title: 'Meetly',
+          }}
+        />
+        
+        <Stack.Screen name="Conferences" component={Conferences} />
+        <Stack.Screen 
+          name="Profile"
+          component={Profile}
+          options={({route}) => ({title: route.params.name})} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
